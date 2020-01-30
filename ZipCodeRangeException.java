e80ac07c2a8222922d5b3aa93388f04b6d594384
sraju76 <60454895+sraju76@users.noreply.github.com>
2020-01-29 17:49:12 -0800

    Add files via upload

---
+++ ZipCodeRangeException.java
@@ -0,0 +1,18 @@
+package com.ws.ziprangeoptimizer.exception;
+
+/**
+ * 
+ * @author skoorella
+ * 
+ * Exception class to handle all input and processing Exceptions.
+ *
+ */
+public class ZipCodeRangeException extends Exception {
+
+	private static final long serialVersionUID = 1L;
+
+	public ZipCodeRangeException(String message) {
+		super(message);
+	}	
+	
+}
